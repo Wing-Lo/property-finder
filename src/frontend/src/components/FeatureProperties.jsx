11e8265afa9d50c9089e9642e62a05040e07107c
wing-lo <15623@coderacademy.edu.au>
2024-08-19 17:02:16 +1000

    properties component layout done

--- src/frontend/src/components/FeatureProperties.jsx
+++ src/frontend/src/components/FeatureProperties.jsx
@@ -1,125 +1,109 @@
 const FeatureProperties = () => {
+    const properties = [
+        {
+            id: 1,
+            title: "Beautiful Apartments",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+            image: "https://bulma.io/images/placeholders/1280x960.png",
+            price: 120000,
+            agent: {
+                firstName: "John",
+                lastName: "Smith",
+            },
+            address: "123 Happy Street",
+            suburb: "Brisbane",
+        },
+        {
+            id: 2,
+            title: "Beautiful Apartments",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+            image: "https://bulma.io/images/placeholders/1280x960.png",
+            price: 120000,
+            agent: {
+                firstName: "John",
+                lastName: "Smith",
+            },
+            address: "123 Happy Street",
+            suburb: "Brisbane",
+        },
+        {
+            id: 3,
+            title: "Beautiful Apartments",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+            image: "https://bulma.io/images/placeholders/1280x960.png",
+            price: 120000,
+            agent: {
+                firstName: "John",
+                lastName: "Smith",
+            },
+            address: "123 Happy Street",
+            suburb: "Brisbane",
+        },
+        {
+            id: 4,
+            title: "Beautiful Apartments",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+            image: "https://bulma.io/images/placeholders/1280x960.png",
+            price: 120000,
+            agent: {
+                firstName: "John",
+                lastName: "Smith",
+            },
+            address: "123 Happy Street",
+            suburb: "Brisbane",
+        },
+    ];
+
     return (
         <section className="section is-medium has-background-white">
-                <h3 className="title is-3">Feature Properties</h3>
-            <div className="columns is-6">
-                <div className="column">
-                    <div className="card">
-                        <div className="card-image">
-                            <figure className="image is-4by3">
-                                <img
-                                    src="https://bulma.io/assets/images/placeholders/1280x960.png"
-                                    alt="Placeholder image"
-                                />
-                            </figure>
-                        </div>
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">
-                                    <figure className="image is-48x48">
+            <h3 className="title is-3 has-text-primary">Feature Properties</h3>
+            <div className="columns is-4 is-multiline">
+                {properties.map((property) => {
+                    return (
+                        <div className="column is-one-third" key={property.id}>
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
                                         <img
-                                            src="https://bulma.io/assets/images/placeholders/96x96.png"
+                                            src="https://bulma.io/assets/images/placeholders/1280x960.png"
                                             alt="Placeholder image"
                                         />
                                     </figure>
                                 </div>
-                                <div className="media-content">
-                                    <p className="title is-4">John Smith</p>
-                                    <p className="subtitle is-6">@johnsmith</p>
+                                <div className="card-content">
+                                    <div className="media">
+                                        <div className="media-content">
+                                            <p className="title is-5 mb-1 has-text-primary">
+                                                {"$" + property.price}
+                                            </p>
+                                            <p className="subtitle is-6">
+                                                {property.address +
+                                                    ", " +
+                                                    property.suburb}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="content">
+                                        {property.description.substring(0, 50) +
+                                            " ..."}
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div className="content">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Phasellus nec iaculis mauris.{" "}
-                                <a>@bulmaio</a>. <a href="#">#css</a>
-                                <a href="#">#responsive</a>
-                                <br />
-                                <time dateTime="2016-1-1">
-                                    11:09 PM - 1 Jan 2016
-                                </time>
+                                <footer className="card-footer">
+                                    <a
+                                        href="#"
+                                        className="card-footer-item has-text-primary"
+                                    >
+                                        View more
+                                    </a>
+                                </footer>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="column">
-                <div className="card">
-                        <div className="card-image">
-                            <figure className="image is-4by3">
-                                <img
-                                    src="https://bulma.io/assets/images/placeholders/1280x960.png"
-                                    alt="Placeholder image"
-                                />
-                            </figure>
-                        </div>
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">
-                                    <figure className="image is-48x48">
-                                        <img
-                                            src="https://bulma.io/assets/images/placeholders/96x96.png"
-                                            alt="Placeholder image"
-                                        />
-                                    </figure>
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-4">John Smith</p>
-                                    <p className="subtitle is-6">@johnsmith</p>
-                                </div>
-                            </div>
-
-                            <div className="content">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Phasellus nec iaculis mauris.{" "}
-                                <a>@bulmaio</a>. <a href="#">#css</a>
-                                <a href="#">#responsive</a>
-                                <br />
-                                <time dateTime="2016-1-1">
-                                    11:09 PM - 1 Jan 2016
-                                </time>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="column">
-                <div className="card">
-                        <div className="card-image">
-                            <figure className="image is-4by3">
-                                <img
-                                    src="https://bulma.io/assets/images/placeholders/1280x960.png"
-                                    alt="Placeholder image"
-                                />
-                            </figure>
-                        </div>
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">
-                                    <figure className="image is-48x48">
-                                        <img
-                                            src="https://bulma.io/assets/images/placeholders/96x96.png"
-                                            alt="Placeholder image"
-                                        />
-                                    </figure>
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-4">John Smith</p>
-                                    <p className="subtitle is-6">@johnsmith</p>
-                                </div>
-                            </div>
-
-                            <div className="content">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Phasellus nec iaculis mauris.{" "}
-                                <a>@bulmaio</a>. <a href="#">#css</a>
-                                <a href="#">#responsive</a>
-                                <br />
-                                <time dateTime="2016-1-1">
-                                    11:09 PM - 1 Jan 2016
-                                </time>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    );
+                })}
             </div>
         </section>
     );

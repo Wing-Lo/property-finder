123ee19ec5706ce0d9a1a169f056003660fd7a82
wing-lo <15623@coderacademy.edu.au>
2024-08-20 14:21:57 +1000

    Layout is done for buy and rent page

---
+++ src/frontend/src/pages/RentPage.jsx
@@ -0,0 +1,14 @@
+
+import Footer from "../components/Footer";
+import FeatureProperties from "../components/FeatureProperties";
+
+const HomePage = () => {
+    return (
+        <>
+            <FeatureProperties sellOrRent={"rent"} />
+            <Footer />
+        </>
+    );
+};
+
+export default HomePage;
